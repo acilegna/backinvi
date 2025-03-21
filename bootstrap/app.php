3e333d845ddd4cf4fd255285b1862c6af8ce406e
acilegna <acilegna.airam88@gmail.com>
2025-03-21 17:54:57 -0600

    config vercel

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+  
